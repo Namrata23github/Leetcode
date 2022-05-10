05d122ef364b92963d448c38b7215332b5a1eb5e
Namrata Agarwal <34562445+Namrata23github@users.noreply.github.com>
2022-05-10 13:56:53 +0530

    Time: 10 ms (5.22%), Space: 42.4 MB (58.74%) - LeetHub

---
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.java
@@ -0,0 +1,47 @@
+class Solution {
+    public boolean isAlienSorted(String[] words, String order) {
+        HashMap<Character, Integer> ordermap = new HashMap();
+        for(int i= 0; i< order.length(); i++) {
+            ordermap.put(Character.valueOf(order.charAt(i)), i);
+        }
+        
+        for (int i = 0; i< words.length - 1; i++) {
+            String one = words[i];
+            String two = words[i+1];
+            
+            if(one.equals(two)) {
+                continue;
+            }
+            
+            int j =0; 
+            
+          
+            int l = Math.min(one.length() ,two.length() );
+            while(j< l) {
+                Character o = Character.valueOf(one.charAt(j));
+                Character t = Character.valueOf(two.charAt(j));
+                if(o!=t ){
+                            if(  ordermap.get(o) > ordermap.get(t) ) {
+                    
+                    System.out.println(o + " " + ordermap.get(o) + " " + t + " " + ordermap.get(t));
+                     System.out.println(o!=t);
+                    return false;
+                } else {
+                             break;   
+                            }
+              
+            }
+        
+                j++;
+        }
+                    
+                if(j == l) {
+                      if(one.length() > two.length()) {
+                return false;
+            }
+                }
+        }
+        
+        return true;
+    }
+}
