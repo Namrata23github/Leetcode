ca35902952ca734ac9d8761c70b831141be6d1ed
Namrata Agarwal <34562445+Namrata23github@users.noreply.github.com>
2022-05-14 16:58:09 +0530

    Time: 17 ms (35.51%), Space: 46.5 MB (76.94%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,15 +1,21 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String, List<String>> map = new HashMap();
+       
+        Map<int [], List<String>> map = new TreeMap<>(new Comparator<int []>() {
+            @Override public int compare(int [] a, int [] b) {
+                for (int i = 0; i < 26; i++) if (a[i] != b[i]) { return (a[i] - b[i]); }
+                return 0;
+            }});
+      
         for(int i=0; i < strs.length; i++) {
             int a[] = new int[26];
           for (char c : strs[i].toCharArray()) {
                 a[c-'a']++;
             }
             
-            List<String> match = map.getOrDefault(Arrays.toString(a), new ArrayList());
+            List<String> match = map.getOrDefault(a, new ArrayList());
             match.add(strs[i]);
-            map.put(Arrays.toString(a),match );
+            map.put(a,match );
         }
        
         return new ArrayList(map.values());
