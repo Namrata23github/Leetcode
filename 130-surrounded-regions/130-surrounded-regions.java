c7f1199915bde416736d20ad1aa638b0188c6f96
Namrata Agarwal <34562445+Namrata23github@users.noreply.github.com>
2022-07-19 18:46:56 +0530

    Time: 5 ms (25.79%), Space: 52.4 MB (16.75%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.java
+++ 130-surrounded-regions/130-surrounded-regions.java
@@ -1,86 +1,102 @@
 class Solution {
     public void solve(char[][] board) {
+        int i = 0;
+        for ( int j = 0; j < board[0].length; j++) {
+            
+      if(board[i][j] == 'O') {
+          
+       board =   dfs(board, i, j);
+      }
         
-        int [][] ds = {{1,0}, {0,1}, {-1,0}, {0, -1}};
-        Stack<int[]> s = new Stack();
+        }
         
-        for(int i = 0; i< board.length; i++) {
-         
+        
+        
+    for ( int j = 0; j < board.length; j++) {
             
-            if(board[i][0] == 'O') {
-                  //System.out.println( "*****1******" );
-                s.push(new int[] { i, 0});
-                dfs(board, s);
+      if(board[j][i] == 'O') {
+          
+         board =  dfs(board, j, i);
+      }
+        
+        }
+        
+        i = board.length - 1;
+        
+    for ( int j = 0; j < board[0].length; j++) {
+            
+      if(board[i][j] == 'O') {
+          
+        board =  dfs(board, i, j );
+      }
+        
+        }
+        
+    i = board[0].length - 1;   
+        
+    for ( int j = 0; j < board.length; j++) {
+            
+      if(board[j][i] == 'O') {
+          
+          board = dfs(board, j, i );
+      }
+        
+        }
+        
+        i = 0;
+        for(  ; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if(board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+                else if(board[i][j] == '2') {
+                     board[i][j] = 'O';
+                }
             }
-            
-               if(board[i][board[0].length - 1] == 'O') {
-                                    // System.out.println( "*****2******" );
-
-                s.push(new int[] { i, board[0].length - 1});
-                dfs(board, s);
-            }
-            
-            
+        }
+        
+       
+        
+        
+    }
+    
+    
+    public char[][] dfs(char[][] board, int i1, int j1) {
+               
+        
            
-        }
-        
-         for(int i = 0; i< board[0].length; i++) {
-                             
-
-                if(board[0][i] == 'O' ) {
-                      //System.out.println( "*****3******" );
-                s.push(new int[] { 0, i});
-                    dfs(board, s);
-                }
+               board[i1][j1] = '2';
+                Stack<int[]> stack = new Stack();
+        stack.push(new int[] {i1, j1});
                 
-            
-             
-               if(board[board.length - 1][i] == 'O') {
-                    // System.out.println( "*****4******" );
-                s.push(new int[] { board.length - 1, i});
-                dfs(board, s);
-            }
-         }
-        
-        
-         for(int i = 0; i< board.length; i++) {
-             for(int j = 0; j< board[0].length; j++) {
-                if(board[i][j] == '1') {
-                    //System.out.println(i + " " + j + "   1" );
-                    board[i][j] = 'O';
-                }
-                 
-                 else if(board[i][j] == 'O') {
-                                        // System.out.println(i + " " + j + "   0" );
-
-                     board[i][j] = 'X';
-                 }
-             }
-         }
-        
-    }
-    
-    
-    void dfs(char[][] board, Stack<int[]> s) {
-        
-        int[][] ds = {{1,0}, {0,1}, {-1, 0}, {0, -1}};
-        
-        while(!s.isEmpty()) {
-            int[] v = s.pop();
-            board[v[0]][v[1]] = '1';
-             //System.out.println( v[0] + " "  + v[1] );
-             
-            for(int[] d : ds) {
-                int r = v[0] + d[0];
-                int c = v[1] + d[1];
+                while(!stack.isEmpty()) {
+                    
+                   int[] arr =  stack.pop();
+                    int i = arr[0];
+                    int j = arr[1];
                 
-                if(r >= 0 && r < board.length && c >= 0 && c < board[0].length && board[r][c] == 'O') {
+                int dir[][] = new int[][] {{1,0}, {-1,0}, {0, 1}, {0,-1}};
+                for (int d[] : dir ) {
+                    int x = i + d[0];
+                     int y = j + d[1];
+                                           
+
+                    if(x > 0 && x < board.length && y > 0 && y < board[0].length && board[x][y] == 'O')
+                    {
+                       
+                        stack.push(new int[] {x ,y});
+                         board[x][y] = '2';
+                        
+                        
+                    }
+                }
+                    
                    
-                    s.push(new int[] { r, c});
-                }
             }
-        }
         
-         //System.out.println( "***********" );
+        return board;
+            
     }
+    
+    
 }
