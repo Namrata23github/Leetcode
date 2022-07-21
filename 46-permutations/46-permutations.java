c5bfd16c363e6019abab6859c104871a0f488c97
Namrata Agarwal <34562445+Namrata23github@users.noreply.github.com>
2022-07-21 12:34:19 +0530

    Time: 3 ms (46.05%), Space: 44.6 MB (61.22%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,30 +1,29 @@
 class Solution {
+    
+    List<List<Integer>> result = new ArrayList();
+
     public List<List<Integer>> permute(int[] nums) {
-        
-       List<List<Integer>> result = new ArrayList();
-       List<Integer> list = new ArrayList();
-       boolean[] freq  = new boolean[nums.length];
-        
-       per(nums, result, list, freq); 
+        backTrack(new ArrayList(), nums);
         return result;
     }
     
-     void per(int[] nums, List<List<Integer>> result, List<Integer> list, boolean[] freq) {
-         
-         if(list.size() == nums.length) {
-            result.add(new ArrayList<>(list));
-             return;
-         }
-         
-         for(int i = 0; i < nums.length; i++) {
-             
-             if(!freq[i]) {
-                 freq[i] = true;
-                 list.add(nums[i]);
-                 per(nums, result, list, freq); 
-                 freq[i] = false;
-                 list.remove(list.size()-1);
-             }
-         }
-     }
+    public void backTrack (List<Integer> list, int[] nums) {
+       
+        if(list.size() == nums.length) {
+            result.add(new ArrayList(list));
+        }
+        
+        for(int i = 0; i < nums.length; i++) {
+            if(list.contains(nums[i]))
+            {
+                continue;
+            }
+            
+            
+            list.add(nums[i]);
+       
+            backTrack(list, nums);
+            list.remove(list.size()-1);
+        }
+    }
 }
