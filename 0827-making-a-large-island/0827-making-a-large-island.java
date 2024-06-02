4d2b5d4c51c8649d967667ff723b87f5529f2efc
Namrata Agarwal <34562445+Namrata23github@users.noreply.github.com>
2024-06-02 22:18:36 +0530

    Time: 73 ms (85.37%), Space: 83.3 MB (21.00%) - LeetHub

---
+++ 0827-making-a-large-island/0827-making-a-large-island.java
@@ -0,0 +1,83 @@
+class Solution {
+    public int largestIsland(int[][] grid) {
+        
+        Map<Character, Integer> map = new HashMap<>();
+        char ch = 'A';
+        
+        for ( int i = 0 ; i < grid.length; i++) {
+             for ( int j = 0 ; j < grid[i].length; j++) {
+                 
+                 if ( grid[i][j] == 1) {
+                    
+                     dfs (  grid , ch,  map ,  i ,  j );
+                         ch = (char)((int)ch + 1);
+                 }
+                 
+             }
+        }
+        
+        int max = 0;
+        int size = 0;
+        
+            for ( int i = 0 ; i < grid.length; i++) {
+             for ( int j = 0 ; j < grid[i].length; j++) {
+                 
+                 if ( grid[i][j] == 0) {
+                     //System.out.println("++++");
+                     HashSet<Character> covered = new HashSet<>();
+                     
+                     
+                     size = 
+                         findSize ( grid, i + 1, j, map, covered) + 
+                         findSize ( grid, i , j + 1, map, covered) + 
+                         findSize ( grid, i - 1, j, map, covered) + 
+                         findSize ( grid, i  , j - 1, map, covered);
+                     max = Math.max ( max, size);
+                 }
+                 
+                 
+             }
+        }
+        
+        return Math.max(max + 1, map.getOrDefault('A', 0) );
+        
+        
+    }
+    
+    public int findSize (int[][] grid  , int i , int j , Map<Character, Integer> map, HashSet<Character> covered ) {
+          if (  i < 0 || j < 0 || i >= grid.length || j >= grid.length || grid[i][j] == 0 || covered.contains((char) grid[i][j])) {
+            return 0 ;
+        }
+        
+        
+        // System.out.println("++++"+ grid[i][j]);
+        
+         covered.add((char) grid[i][j]);
+        return map.get((char)grid[i][j]);
+        
+        
+        
+    }
+    
+    void dfs ( int[][] grid , char ch, Map<Character, Integer> map , int i , int j ) {
+        
+        if ( i < 0 || j < 0 || i >= grid.length || j >= grid.length || grid[i][j] != 1) {
+            return ;
+        }
+         
+           int size = map.getOrDefault(ch,0 ) + 1;
+            map.put(ch , size);
+        
+        
+            grid[i][j] = ch;
+            
+            dfs (grid, ch, map, i + 1, j);
+            dfs (grid, ch, map, i, j - 1);
+            dfs (grid, ch, map, i , j + 1);
+            dfs (grid, ch, map, i - 1, j );
+        
+        
+    }
+    
+    
+}
